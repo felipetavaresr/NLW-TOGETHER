889e9e25af465cd589881e7cd131ea3b2a2fdd41
Felipe Tavares <felipe.tavaresk@gmail.com>
2021-06-24 13:48:33 -0300

    pages created, .css added and upgrade in archives

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { Button } from './components/Button';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { NewRoom } from './pages/NewRoom';
+import { Home } from './pages/Home';
 
 function App() {
   return (
-  <div>
-   <Button />
-   <Button />
-   <Button />
-   <Button />
-   <Button />
-   <Button />
-   <Button />
-   <Button />
-  </div>
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
